15fec2971c87131ca8e69d205e47d197d1f856f2
Alice088 <alien280579@gmail.com>
2026-03-15 14:20:32 +0300

    Add file size check and extra error handling

--- internal/http/v1/load/load.go
+++ internal/http/v1/load/load.go
@@ -38,6 +38,13 @@ func (h *Handler) Load() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
 
+		if r.ContentLength > size.MB5 {
+			httpx.HttpResponse(w, http.StatusRequestEntityTooLarge, map[string]string{
+				"error": "file too large",
+			})
+			return
+		}
+
 		header := make([]byte, 5)
 		if _, err := io.ReadFull(r.Body, header); err != nil {
 			httpx.HttpResponse(w, http.StatusBadRequest, map[string]string{
@@ -55,7 +62,7 @@ func (h *Handler) Load() http.HandlerFunc {
 
 		reader := io.MultiReader(
 			bytes.NewReader(header),
-			io.LimitReader(r.Body, size.MB5-5),
+			r.Body,
 		)
 
 		objectSize := r.ContentLength
@@ -94,7 +101,7 @@ func (h *Handler) Load() http.HandlerFunc {
 
 		job, err := h.Queries.CreateJob(ctx, queries.CreateJobParams{
 			ID: pgtype.UUID{
-				Bytes: uuid.New(),
+				Bytes: pdfUUID,
 				Valid: true,
 			},
 			ObjectKey: pdfUUID.String() + ".pdf",
@@ -106,15 +113,34 @@ func (h *Handler) Load() http.HandlerFunc {
 			httpx.HttpResponse(w, http.StatusInternalServerError, map[string]string{
 				"error": "failed to process file",
 			})
+
+			ctx, cancel = context.WithTimeout(r.Context(), h.Timeout)
+			defer cancel()
+			if err := h.MinIO.RemoveObject(ctx, h.BucketName, pdfUUID.String()+".pdf", minio.RemoveObjectOptions{}); err != nil {
+				h.Logger.Error("Failed to remove object during failed create job", "error", err.Error())
+			}
 			return
 		}
 
 		if bytes.Compare(job.ID.Bytes[:], pdfUUID[:]) != 0 {
-			h.Logger.Error("Not same job UUID", "error", err.Error())
+			h.Logger.Error("Not same job UUID")
 
 			httpx.HttpResponse(w, http.StatusInternalServerError, map[string]string{
 				"error": "failed to process file",
 			})
+
+			ctx, cancel = context.WithTimeout(r.Context(), h.Timeout)
+			defer cancel()
+			if err := h.MinIO.RemoveObject(ctx, h.BucketName, pdfUUID.String()+".pdf", minio.RemoveObjectOptions{}); err != nil {
+				h.Logger.Error("Failed to remove object during compare job id", "error", err.Error())
+			}
+
+			ctx, cancel = context.WithTimeout(r.Context(), h.Timeout)
+			defer cancel()
+			h.Queries.DeleteJob(ctx, job.ID)
+			if err := h.MinIO.RemoveObject(ctx, h.BucketName, pdfUUID.String()+".pdf", minio.RemoveObjectOptions{}); err != nil {
+				h.Logger.Error("Failed to remove object during compare job id", "error", err.Error())
+			}
 			return
 		}
 
