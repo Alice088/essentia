56ef2aec4c28eff780f02b40af29b3fcdf876e54
Alice088 <alien280579@gmail.com>
2026-03-21 13:50:50 +0300

    fix config

--- pkg/env/config.go
+++ pkg/env/config.go
@@ -21,10 +21,10 @@ type DB struct {
 }
 
 type MinIO struct {
-	Endpoint  string `env:"ENDPOINT,required"`
-	AccessKey string `env:"ACCESS_KEY,required"`
-	SecretKey string `env:"SECRET_KEY,required"`
-	SSL       bool   `env:"SSL,required"`
-	Location  string `env:"LOCATION,required"`
-	PDFBucket string `env:"PDF_BUCKET,required"`
+	Endpoint  string `env:"MINIO_ENDPOINT,required"`
+	AccessKey string `env:"MINIO_ACCESS_KEY,required"`
+	SecretKey string `env:"MINIO_SECRET_KEY,required"`
+	SSL       bool   `env:"MINIO_SSL,required"`
+	Location  string `env:"MINIO_LOCATION,required"`
+	PDFBucket string `env:"MINIO_PDF_BUCKET,required"`
 }
