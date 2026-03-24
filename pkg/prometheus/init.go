08e6dcf1c06d01f2eaa6a5b94f97678ef5fc4247
Alice088 <alien280579@gmail.com>
2026-03-24 15:25:24 +0300

    add parsing_total.go metric to prometheus

--- pkg/prometheus/init.go
+++ pkg/prometheus/init.go
@@ -1,13 +1,14 @@
 package prometheus
 
 import (
-	metrics2 "Alice088/essentia/pkg/prometheus/metrics"
+	"Alice088/essentia/pkg/prometheus/metrics"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 func UpMetrics() {
-	prometheus.MustRegister(metrics2.HttpRequestsTotal)
-	prometheus.MustRegister(metrics2.HttpRequestsInFlight)
-	prometheus.MustRegister(metrics2.HttpRequestTotalDuration)
+	prometheus.MustRegister(metrics.HttpRequestsTotal)
+	prometheus.MustRegister(metrics.HttpRequestsInFlight)
+	prometheus.MustRegister(metrics.HttpRequestTotalDuration)
+	prometheus.MustRegister(metrics.ParsingTotal)
 }
---
+++ pkg/prometheus/metrics/parsing_total.go
@@ -0,0 +1,19 @@
+package metrics
+
+import "github.com/prometheus/client_golang/prometheus"
+
+// ParsingStatus = Failed | Success
+type ParsingStatus = string
+
+const (
+	Failed  ParsingStatus = "failed"
+	Success ParsingStatus = "success"
+)
+
+var ParsingTotal = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "parsing_total",
+		Help: "Total number of PDF parsing tasks",
+	},
+	[]ParsingStatus{"status"},
+)
