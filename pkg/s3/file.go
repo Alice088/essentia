07bfe8642e66f8d91fa76246bd89538b3b2cdc4c
Alice088 <alien280579@gmail.com>
2026-03-30 09:30:03 +0300

    refactor(system): Useless elements was deleted

---
+++ pkg/s3/file.go
@@ -0,0 +1,21 @@
+package storage
+
+import "encoding/json"
+
+type File struct {
+	Name string
+	Ext  string
+	Path string
+	JSON json.RawMessage
+}
+
+func NewJSON(name, path string, object any) File {
+	bytes, _ := json.Marshal(object)
+
+	return File{
+		Name: name,
+		Ext:  "json",
+		Path: path,
+		JSON: bytes,
+	}
+}
